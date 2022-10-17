1bd297365309ba16b95c1815e27ba35f134b51a5
psimosis <psimosis@gmail.com>
2022-10-17 19:16:51 -0300

    Update

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import imagen from './img/mesaayuda.png'
+
+
 
 export default function App() {
   return (
+    
+    /* Contenedor */
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      
+      {/* Imgaen */}
+      <Image
+        source={imagen}
+        style={styles.image}
+      />
+
+      {/* Textos */}
+      <Text style={styles.title}>IT Support-e</Text>
+      <Text style={styles.subtitle}>Bienvenidos a la app de Soporte IT</Text>
+
+      {/* Botones */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert('Hello')}
+      >
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+      
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -13,8 +39,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dimgray',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  title:{
+    fontSize: 30,
+    color: 'white',
+  },
+  subtitle:{
+    fontSize: 20,
+    color: 'red',
+  },
+  image: {
+    height:300,
+    width:300,
+    borderRadius: 150,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "red",
+    padding: 20,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  }
+
 });
