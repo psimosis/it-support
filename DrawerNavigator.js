74b84f5dddfaef1fd66edf2caf77772dcf3e11d1
psimosis <psimosis@gmail.com>
2022-10-25 21:40:59 -0300

    Add MisTickets Screen

--- DrawerNavigator.js
+++ DrawerNavigator.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import NewTicketScreen from "./screens/NewTicketScreen";
+import MisTickets from "./screens/MisTickets";
 
 const Drawer = createDrawerNavigator()
 
 export function DrawerNavigator() {
     return(
         <Drawer.Navigator>
-            <Drawer.Screen name = "NewTicketScreen" component = {NewTicketScreen}/>
+            <Drawer.Screen name = "Mis Tickets" component = {MisTickets}/>
+            <Drawer.Screen name = "Nuevo Ticket" component = {NewTicketScreen}/>
         </Drawer.Navigator>
     )
 }
---
+++ screens/MisTickets.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text,View} from 'react-native';
+
+const MisTickets = () => {
+   
+    return (
+    
+        <View>
+            <Text>Mis tickets </Text>
+           
+        </View>
+
+    );
+};
+
+export default MisTickets;
