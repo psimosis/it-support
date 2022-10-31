7fec6692152f37e5eefce7c80ebace35772b18de
psimosis <psimosis@gmail.com>
2022-10-31 16:57:58 -0300

    Custom Drawer

--- DrawerNavigator.js
+++ DrawerNavigator.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {createDrawerNavigator, DrawerContentScrollView} from "@react-navigation/drawer";
 import NewTicketScreen from "./screens/NewTicketScreen";
 import MisTickets from "./screens/MisTickets";
 import { View,StyleSheet } from "react-native-web";
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const Drawer = createDrawerNavigator()
 
@@ -14,14 +15,48 @@ export function DrawerNavigator() {
                     name = "Mis Tickets" 
                     component = {MisTickets}
                     options={{
+                        drawerActiveTintColor: '#2813F3',
                         headerTitleStyle: {fontFamily: 'normal'},
                         drawerLabel: "Mis Tickets",
                         drawerItemStyle: {backgroundColor: 'lightgrey'},
-                    }}
+                        drawerIcon: ({focused, size}) => (<Ionicons
+                                name="list"
+                                size={size}
+                                color={focused ? '#2813F3' : '#000000'}
+                            />
+                        ),
+                    }}  
                 />
 
-                <Drawer.Screen name = "Crear Ticket" component = {NewTicketScreen}/>
-                
+                <Drawer.Screen name = "Crear Ticket" 
+                component = {NewTicketScreen}
+                options={{
+                    drawerActiveTintColor: '#2813F3',
+                    headerTitleStyle: {fontFamily: 'normal'},
+                    drawerLabel: "Crear Ticket",
+                    drawerItemStyle: {backgroundColor: 'lightgrey'},
+                    drawerIcon: ({focused, size}) => (<Ionicons
+                            name="duplicate-outline"
+                            size={size}
+                            color={focused ? '#2813F3' : '#000000'}                        />
+                    ),
+                }}  />
+
+                <Drawer.Screen name = "Cerrar Sesion" 
+                component = {NewTicketScreen}
+                options={{
+                    drawerActiveTintColor: '#2813F3',
+                    headerTitleStyle: {fontFamily: 'normal'},
+                    drawerLabel: "Cerrar Sesion",
+                    drawerItemStyle: {backgroundColor: 'lightgrey'},
+                    drawerIcon: ({focused, size}) => (<Ionicons
+                            name="exit-outline"
+                            size={size}
+                            color={focused ? '#2813F3' : '#000000'}                        />
+                    ),
+                }}  />
+
+            
         </Drawer.Navigator>
     )
 }
@@ -29,7 +64,7 @@ export function DrawerNavigator() {
 const styles = StyleSheet.create({
     separador: {
         marginVertical: 8,
-        borderBottomColor: "Blue",
+        borderBottomColor: "blue",
         borderBottomWidth: StyleSheet.hairlineWidth
     }
 
